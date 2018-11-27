14d9f29843b18ddd903dcbc7723c70200af6b388
miun173 <fahmifan173@gmail.com>
2018-11-27 18:28:24 +0700

    finished shorten url API

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 'use strict';
 
+require('dotenv').config()
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
 var cors = require('cors');
+const bodyParser = require('body-parser');
+const { isURL } = require('./utils')
 
 var app = express();
+app.use(bodyParser.urlencoded());
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useMongoClient: true,
+});
 
 app.use(cors());
 
@@ -21,16 +25,65 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// root views
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+// make a Schema
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  original: String,
+  shorten: {type:Number,default:0},
+})
+
+// make a Model
+const ShortenURL = mongoose.model("ShortenURL", urlSchema) 
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", (req, res) => {
+  // validate the url
+  if(!isURL(req.body.url)) return res.json({"error":"invalid URL"})
+
+  // count datas to get new `shorten value`
+  ShortenURL.count({}, (error, urls) => {
+    console.log("urls", urls)
+
+    // insert to the Model
+    const shortenURL = new ShortenURL({
+      original: req.body.url,
+      shorten: urls + 1
+    }) 
+
+    shortenURL.save((error, el) => {
+      if(error) return res.json({error: error})
+
+      return res.json({
+        original: el.original,
+        shorten: el.shorten,
+      })
+    })
+  })
 });
 
+app.get("/api/shorturl/:shorten?", async (req, res) => {
+  // validate
+  if(req.params.shorten === "") {
+    return
+  }
+
+  // query from the DB
+  ShortenURL.findOne({shorten: req.params.shorten}, (error, shortenURL) => {
+    if(error) {
+      console.log("Error", error)    
+      return res.json({error: error})
+    }
+
+    return (shortenURL.original === "") 
+      ? null
+      : res.redirect(shortenURL.original)
+  })
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
---
+++ utils.js
@@ -0,0 +1,6 @@
+function isURL(url) {
+    const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(url) ? true : false
+  }
+  
+  exports.isURL = isURL
